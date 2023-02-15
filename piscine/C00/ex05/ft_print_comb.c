bb83030a27d0b9dc130ce9c463acdc752483458a
Giovanni De Vito <gde-vito@e3r3p1.42roma.it>
2023-02-15 22:54:48 +0100

    added C00

---
+++ piscine/C00/ex05/ft_print_comb.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-vito <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 18:54:57 by gde-vito          #+#    #+#             */
+/*   Updated: 2023/02/15 18:57:01 by gde-vito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(int a, int b, int c)
+{
+	ft_putchar(a + 48);
+	ft_putchar(b + 48);
+	ft_putchar(c + 48);
+	ft_putchar(',');
+	ft_putchar(' ');
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	b = 1;
+	c = 2;
+	while (a <= 7)
+	{
+		while (b <= 8)
+		{
+			while (c <= 9)
+			{
+				ft_print(a, b, c++);
+			}
+			c = ++b;
+			c++;
+		}	
+		b = ++a;
+		c = ++b;
+		c++;
+	}
+}
