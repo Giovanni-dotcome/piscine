d092a1233fe89b59b9096cea2841d2bb0f48b5bd
Giovanni-dotcome <giodevito99@gmail.com>
2023-02-15 07:44:17 +0100

    ex05

--- piscine/C00/ex05/ft_print_comb.c
+++ piscine/C00/ex05/ft_print_comb.c
@@ -1,6 +1,28 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void ft_print_comb(void)
 {
+	int a;
+	int b;
+	int c;
 
+	a = 0;
+	b = 1;
+	c = 2;
+	while (a <= 7)
+	{
+		while (b <= 8)
+		{
+			while (c <= 9)
+			{
+				printf("%d%d%d, ", a, b, c++);
+			}
+			c = ++b;
+			c++;
+		}	
+		b = ++a;
+		c = ++b;
+		c++;
+	}
 }
