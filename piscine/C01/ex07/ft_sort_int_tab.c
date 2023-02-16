aeba3350b93eee9815f894ee365a5ce0716160f5
Giovanni De Vito <gde-vito@e3r3p1.42roma.it>
2023-02-16 09:25:48 +0100

    ex07

--- piscine/C01/ex07/ft_sort_int_tab.c
+++ piscine/C01/ex07/ft_sort_int_tab.c
@@ -6,10 +6,37 @@
 /*   By: gde-vito <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 22:57:58 by gde-vito          #+#    #+#             */
-/*   Updated: 2023/02/15 23:03:19 by gde-vito         ###   ########.fr       */
+/*   Updated: 2023/02/16 09:22:50 by gde-vito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int	x;	
+	int	y;	
+	int	temp;	
+
+	x = 0;
+	while (x < size - 1)
+	{
+		y = 0;
+		while (y < size - 1)
+		{
+			if (tab[y] > tab[y + 1])
+			{
+				ft_swap(&tab[y], &tab[y + 1]);
+			}
+			y++;
+		}
+		x++;
+	}
 }
