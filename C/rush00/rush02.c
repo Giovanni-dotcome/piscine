5b871681adc9d45f498ac804e7cc12b2fb2817d4
Giovanni De Vito <gde-vito@e3r3p1.42roma.it>
2023-02-17 22:14:45 +0100

    rush00

--- C/rush00/rush02.c
+++ C/rush00/rush02.c
@@ -6,10 +6,42 @@
 /*   By: gde-vito <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:20:23 by gde-vito          #+#    #+#             */
-/*   Updated: 2023/02/17 19:45:36 by gde-vito         ###   ########.fr       */
+/*   Updated: 2023/02/17 22:14:14 by gde-vito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_putchar.c"
+
+void	print_external_row(int w)
+{
+	int	i;
+
+	i = 0;
+	ft_putchar("A");
+	while (i < w-2)
+	{
+		ft_putchar("B");
+		i++;
+	}
+	if (w > 1)
+		ft_putchar("A");
+	ft_putchar("\n");
+}
+
+void	print_internal_row(int w)
+{
+	int	i;
+
+	i = 0;
+	ft_putchar("B");
+	while (i < w-2)
+	{
+		ft_putchar(" ");
+		i++;
+	}
+	if (w > 1)
+		ft_putchar("B");
+	ft_putchar("\n");
+}
 
 void	rush(int w, int l)
 {
@@ -18,6 +50,10 @@ void	rush(int w, int l)
 	i = 0;
 	print_external_row(w);
 	while (i < l - 2)
+	{
 		print_internal_row(w);
-	print_external_row(w);
+		i++;
+	}
+	if (l > 1)
+		print_external_row(w);
 }
