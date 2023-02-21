4969217fe6004282d0402afa0b565531568946a0
Giovanni De Vito <gde-vito@e3r4p4.42roma.it>
2023-02-21 20:43:30 +0100

    ex05

---
+++ C/C03/ex05/ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-vito <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 10:02:41 by gde-vito          #+#    #+#             */
+/*   Updated: 2023/02/21 15:29:41 by gde-vito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_strlen(char *c)
+{
+	int i;
+
+	i = 0;
+	while (c[i++]) ;
+	return (i - 1);
+}
+
+unsigned int	ft_strlcat(char *dst, char *src, unsigned int size)
+{
+	int	d_len;
+	int	s_len;
+	int	offset;
+	int	src_ind;
+	int i = 0; //size - d_len - 1;
+	d_len = ft_strlen(dst);
+	s_len = ft_strlen(src);
+	offset = d_len;
+	src_ind = 0;
+	while (src[src_ind] && i < size - d_len - 1)
+		dst[offset++] = src[src_ind++];
+	dst[offset] = 0;
+	return (d_len + s_len);
+}
